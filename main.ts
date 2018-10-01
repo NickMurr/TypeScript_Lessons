474152d22e9ae2602329d8ece01619e19badaf94
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-10-01 21:51:04 +0300

    Variable Types

--- main.ts
+++ main.ts
@@ -6,3 +6,55 @@ const x = 10;
 const y = 20;
 
 const title = 'Coderevolution';
+
+const isBeginner: boolean = true;
+const total: number = 0;
+const name: string = 'Mykyta';
+const sentence:string = `my name is ${name}
+I am beginner in TypeScript`;
+
+console.log(sentence);
+
+const n: null = null;
+const u: undefined = undefined;
+
+const isNew: boolean = null;
+const myName: string = undefined;
+
+const list1: number[] = [1, 2, 3];
+const list2: Array<number> = [1, 2, 3];
+
+const person1: [string, number] = ['Chris', 35];
+
+enum Color {Red= 55, Green, Blue}
+
+const c: Color = Color.Green;
+console.log(c);
+
+let randomValue: any = 10;
+randomValue = true;
+randomValue = 'Nick';
+const myVariable: unknown = 10;
+
+function hasName(obj:any):obj is {name:string} {
+  return !!obj &&
+    typeof obj === 'object' &&
+    'name' in obj;
+}
+if (hasName(myVariable)) {
+  console.log(myVariable.name);
+}
+(myVariable as string).toUpperCase();
+
+let a;
+a = 10;
+a = true;
+
+const b = 20;
+
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+let anyType: any;
+anyType = 20;
