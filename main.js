f10e0008f70431acd8faf8052f208ee0a8ebf8b2
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-10-01 14:18:57 +0300

    Type Script init

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var message = 'Welcome back';
+console.log(message);
+var x = 10;
+var y = 20;
+var title = 'Coderevolution';
---
+++ main.ts
@@ -0,0 +1,8 @@
+export {};
+const message = 'Welcome back';
+console.log(message);
+
+const x = 10;
+const y = 20;
+
+const title = 'Coderevolution';
